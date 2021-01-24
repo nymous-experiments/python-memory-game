9364c69f87c1daee27fcabeae0fde5c7f758f252
Alex <alex.lin@centrale.centralelille.fr>
2021-01-24 17:32:15 +0100

    traduction des règles

--- intro.py
+++ intro.py
@@ -20,9 +20,9 @@ def choose_mode():
 2. Si les deux cartes sont identiques, elle resteront face visibles.
 3. Sinon elles seront masquées.
 4. Le jeu se termine lorsque toutes les cartes sont visibles.
-5. A new game starts automatically.
-6. Press ENTER to change mode.
-7. Press ESCAPE to exit.
+5. Une nouvelle partie commence automatiquement.
+6. Appuyez entrer pour changer de MODE.
+7. Appuyez ECHAP pour sortir.
         """,
         width="60",
         bg="misty rose",
